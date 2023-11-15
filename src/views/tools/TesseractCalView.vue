31afe0847d2a3af74d64483499e36c4450df2feb
yi <guiyc@outlook.com>
2023-11-15 16:47:09 +0800

    fix getserviceMonth

--- src/views/tools/TesseractCalView.vue
+++ src/views/tools/TesseractCalView.vue
@@ -348,6 +348,7 @@ function getServiceMonths(dateArray) {
     const formattedMonth = `${year}${month.toString().padStart(2, '0')}`;
     monthsArray.push(formattedMonth);
     currentDate.setMonth(currentDate.getMonth() + 1);
+    currentDate.setDate(1)
 
     if (year == 2020){
       console.log(endDate)

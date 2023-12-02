1b29075154eabd25adfff98b47d6c52d41ecc040
yi <guiyc@outlook.com>
2023-12-02 12:51:06 +0800

    add prettierrc

--- src/components/DateRangePicker.vue
+++ src/components/DateRangePicker.vue
@@ -3,19 +3,18 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed} from 'vue'
-import { DatePicker } from 'ant-design-vue'
-import { Dayjs } from 'dayjs';
+import { ref, computed } from "vue";
+import { DatePicker } from "ant-design-vue";
+import { Dayjs } from "dayjs";
 const monthSelect = ref<Dayjs>();
-const monthRange = computed(()=>{
-  const firstDay = monthSelect.value.startOf('month').format('YYYY-MM-DD');
-  const lastDay = monthSelect.value.endOf('month').format('YYYY-MM-DD');
-  console.log(firstDay)
+const monthRange = computed(() => {
+  const firstDay = monthSelect.value.startOf("month").format("YYYY-MM-DD");
+  const lastDay = monthSelect.value.endOf("month").format("YYYY-MM-DD");
+  console.log(firstDay);
   return [firstDay, lastDay];
-
-})
+});
 defineExpose({
   monthSelect,
-  monthRange
-})
+  monthRange,
+});
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from "./App.vue";
+import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+const app = createApp(App);
+app.use(Antd);
+app.use(createPinia());
+app.use(router);
 
-import App from './App.vue'
-import router from './router'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/reset.css';
-const app = createApp(App)
-app.use(Antd)
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
